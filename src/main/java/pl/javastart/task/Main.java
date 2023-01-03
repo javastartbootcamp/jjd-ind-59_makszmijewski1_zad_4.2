fd7457280de7647f9e08208963b2883c5d2d933c
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-03 19:46:47 +0100

    pierwszy commit

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Person person1 = new Person("Maksymilian", "Żmijewski");
+        Tasks task1 = new Tasks("programming", "creating programs", 1, person1);
+
+        Person person2 = new Person("Jan", "Kowalski");
+        Tasks task2 = new Tasks("restoration", "restoration old cars",
+                 1, person2);
+
+        Tasks task3 = new Tasks("cleaning", "cleaning house");
+
+        Tasks task4 = new Tasks("playing", "playing cards", 0);
+
+        System.out.println(task1.highPriority());
+        System.out.println(task2.mediumPriority());
+        System.out.println(task4.mediumPriority());
+
     }
 }
